b54e594b22ed5456bb2373ed52970917cdb39b61
soundn <soundnwankwo@gmail.com>
2024-05-07 21:28:48 +0100

    add

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.28.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "test-422618"
+  region = "us-central1"
+  zone = "us-central1-a"
+
+}
